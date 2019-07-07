ddfa0f1458a18c199216273002210cafefc87964
Igor Sochyvets <isochivets@gmail.com>
2019-07-07 19:18:31 +0300

    All Basic moved to separate folder

---
+++ basic/basic1.py
@@ -0,0 +1,7 @@
+def greet(name):
+        s = "Hello World, " + name
+        return s
+
+name1 = input('Enter Name: ')
+s1 = greet(name1)
+print(s1)
--- basic1.py
+++
@@ -1,7 +0,0 @@
-def greet(name):
-        s = "Hello World, " + name
-        return s
-
-name1 = input('Enter Name: ')
-s1 = greet(name1)
-print(s1)
